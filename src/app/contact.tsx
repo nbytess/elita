e4d7d099f761ca14ebbbcfb7fa9d1e5285780f96
BuildTools <unconfigured@null.spigotmc.org>
2024-02-19 23:27:12 +0100

    dasdasx

--- src/app/contact.tsx
+++ src/app/contact.tsx
@@ -10,7 +10,7 @@ import { ContactIconsList } from './contactIcons';
 export function Contact() {
     return (
         <Container className={classes.wrapper} id="kontakt">
-          <Stack align="center" spacing="xl">
+          <Stack align="center" spacing={40}>
               <Title className={classes.title} order={2}>Kontakt:</Title>
               <ContactIconsList />
           </Stack>
